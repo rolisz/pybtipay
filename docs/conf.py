e7ed022568133479d5c0b1aa5798a8185bbacbf1
Roland Szabo <rolisz@gmail.com>
2022-02-08 10:28:34 +0200

    Bump to 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'BTIPay'
 year = '2021'
 author = 'Roland Szabo'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.4'
+version = release = '0.0.5'
 
 pygments_style = 'trac'
 templates_path = ['.']
